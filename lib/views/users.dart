12fbdb212c37cec7b03df81f0c26ec016f0d0b73
esraaabdrabo <esraaabdrabo23@gmail.com>
2022-07-16 12:46:50 +0200

    create user screen

---
+++ lib/views/users.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class users extends StatefulWidget {
+  const users({Key? key}) : super(key: key);
+
+  @override
+  State<users> createState() => _usersState();
+}
+
+class _usersState extends State<users> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
